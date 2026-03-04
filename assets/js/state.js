72d5d6d76c992893a6b94ad3746104f53696c318
Mélarthur <amontandon01@gmail.com>
2026-03-04 09:57:13 +0100

    Update defaultSegments with new time ranges and values

--- assets/js/state.js
+++ assets/js/state.js
@@ -9,6 +9,7 @@ export const getNum = (el) => parseFloat(el?.value) || 0;
 export const getInt = (el) => parseInt(el?.value, 10) || 0;
 
 export const defaultSegments = () => ([
-  { id: uuid(), from: 2025, to: 2039, monthly: 3340, annual: 7500 },
-  { id: uuid(), from: 2040, to: 2060, monthly: 0, annual: 0 },
+  { id: uuid(), from: 2025, to: 2026, monthly: 1000, annual: 6000 },
+  { id: uuid(), from: 2026, to: 2027, monthly: 2340, annual: 7500 },
+  { id: uuid(), from: 2027, to: 2039, monthly: 3000, annual: 8000 },
 ]);
